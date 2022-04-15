f78d15a22c72c4c9790abf885855fe0cc705103b
y836475809 <tttdaytttday@gmail.com>
2022-04-15 23:29:30 +0900

    整理

--- Assets/Script/Snake.cs
+++ Assets/Script/Snake.cs
@@ -11,8 +11,6 @@ public class Snake : MonoBehaviour {
 	//Did user died?
 	bool isDied = false;
 
-	//bool col_ava = true;
-
 	// Tail Prefab
 	public GameObject tailPrefab;
 
@@ -32,60 +30,14 @@ public class Snake : MonoBehaviour {
 	void Start () {
 		starts = new Vector3[raysCount];
 		ends = new Vector3[raysCount];
-		// Move the Snake every 300ms
-		//InvokeRepeating("Move", 0.3f, 0.3f);
     }
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (!isDied) {
-			var rot = 0f;
-			// Move in a new Direction?
-			if (Input.GetKey(KeyCode.RightArrow)) {
-				dir = Vector2.right;
-				rot = -0.3f;
-			} else if (Input.GetKey(KeyCode.DownArrow)) {
-				dir = -Vector2.up;    // '-up' means 'down'
-			} else if (Input.GetKey(KeyCode.LeftArrow)) {
-				dir = -Vector2.right; // '-right' means 'left'
-				rot = 0.3f;
-			} else if (Input.GetKey(KeyCode.UpArrow)) {
-				dir = Vector2.up;
-			}
-			float speed = 10;
-			transform.Rotate(0, 0, rot*5);
-			Vector3 velocity = transform.rotation * new Vector3(0, 1, 0);
-			transform.position += velocity* speed * Time.deltaTime;
-
-			CalculateEnds();
-
-			for (int i = 0; i < raysCount; i++) {
-				var hit = Physics2D.Linecast(starts[i], ends[i]);
-				if (hit.collider != null) {
-					Debug.DrawLine(starts[i], ends[i], Color.red);
-                } else {
-					Debug.DrawLine(starts[i], ends[i], Color.white);
-				}
-			}
-
-			Move();
-
-			var head = transform;
-			for (int i = 0; i < tail.Count; i++) {
-				var pos = head.position;
-				var diff_pos = pos - tail[i].position;
-				var new_r = getAngle(diff_pos.x, diff_pos.y);
-				var rad = getRad(diff_pos.x, diff_pos.y);
-				var new_x = pos.x - Mathf.Cos(rad) * 2.5f;
-				var new_y = pos.y - Mathf.Sin(rad) * 2.5f;
-				tail[i].position = new Vector3(new_x, new_y, 0);
-				tail[i].rotation = Quaternion.Euler(0, 0, -(90f - new_r));
-				head = tail[i];
-			}
-		} else {
-			if (Input.GetKey(KeyCode.R)){
+        if (isDied) {
+			if (Input.GetKey(KeyCode.R)) {
 				//clear the tail
-				foreach(var t in tail) {
+				foreach (var t in tail) {
 					Destroy(t.gameObject);
 				}
 				tail.Clear();
@@ -97,7 +49,53 @@ public class Snake : MonoBehaviour {
 				//make snake alive
 				isDied = false;
 			}
+			return;
 		}
+
+		var rot = 0f;
+		// Move in a new Direction?
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			dir = Vector2.right;
+			rot = -0.3f;
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			dir = -Vector2.up;    // '-up' means 'down'
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			dir = -Vector2.right; // '-right' means 'left'
+			rot = 0.3f;
+		} else if (Input.GetKey(KeyCode.UpArrow)) {
+			dir = Vector2.up;
+		}
+		float speed = 10;
+		transform.Rotate(0, 0, rot*5);
+		Vector3 velocity = transform.rotation * new Vector3(0, 1, 0);
+		transform.position += velocity* speed * Time.deltaTime;
+
+		CalculateEnds();
+
+		for (int i = 0; i < raysCount; i++) {
+			var hit = Physics2D.Linecast(starts[i], ends[i]);
+			if (hit.collider != null) {
+				Debug.DrawLine(starts[i], ends[i], Color.red);
+            } else {
+				Debug.DrawLine(starts[i], ends[i], Color.white);
+			}
+		}
+
+		Move();
+
+		var head = transform;
+		foreach(var t in tail) {
+			var pos = head.position;
+			var diff_pos = pos - t.position;
+			var new_r = getAngle(diff_pos.x, diff_pos.y);
+			var rad = getRad(diff_pos.x, diff_pos.y);
+			var new_x = pos.x - Mathf.Cos(rad) * 2.5f;
+			var new_y = pos.y - Mathf.Sin(rad) * 2.5f;
+			t.position = new Vector3(new_x, new_y, 0);
+			t.rotation = Quaternion.Euler(0, 0, -(90f - new_r));
+			head = t;
+		}
+
 	}
 
 	float getAngle(float x, float y) {
@@ -113,38 +111,34 @@ public class Snake : MonoBehaviour {
         return Mathf.Atan2(y, x);
 	}
 	void Move() {
-		if (!isDied) {
-			if (ate) {
-				//col_ava = false;
-				Vector2 v = transform.position;
-				var qq = transform.rotation;
-				float speed = 3f;
-				Vector3 velocity = transform.rotation * new Vector3(0, speed, 0);
-				Vector2 v2 = new Vector2(v.x - velocity.x, v.y - velocity.y);
-                if (tail.Count > 0) {
-					var ll = tail[tail.Count - 1];
-					v = ll.position;
-					qq = ll.rotation;
-					velocity = ll.rotation * new Vector3(0, speed, 0);
-					v2 = new Vector2(v.x - velocity.x, v.y - velocity.y);
-				}
-				// Load Prefab into the world
-				GameObject g = (GameObject)Instantiate (tailPrefab,
-								  v2,
-								  qq);
-                tail.Add(g.transform);
-                // Reset the flag
-                ate = false;
-            } else if (tail.Count > 0) {	// Do we have a Tail?
-					//// Move last Tail Element to where the Head was
-					//tail.Last ().position = v;
+		if(isDied) {
+			return;
+		}
 
-					//// Add to front of list, remove from the back
-					//tail.Insert (0, tail.Last ());
-					//tail.RemoveAt (tail.Count - 1);
-			}
+		if (ate) {
+			AddTail();
+			// Reset the flag
+			ate = false;
         }
 	}
+
+	void AddTail() {
+		var pre_pos = transform.position;
+		var pre_rot = transform.rotation;
+		if (tail.Count > 0) {
+			var pre_tail = tail[tail.Count - 1];
+			pre_pos = pre_tail.position;
+			pre_rot = pre_tail.rotation;
+		}
+
+		const float speed = 3f;
+		var velocity = pre_rot * new Vector3(0, speed, 0);
+		var pos = new Vector3(pre_pos.x - velocity.x, pre_pos.y - velocity.y, 0);
+
+		GameObject g = (GameObject)Instantiate(tailPrefab, pos, pre_rot);
+		tail.Add(g.transform);
+	}
+
 
 	void CalculateEnds() {
 		float step = 180f / (raysCount - 1);
@@ -175,9 +169,6 @@ public class Snake : MonoBehaviour {
 			Destroy(coll.gameObject);
 		} else {    // Collided with Tail or Border
 			isDied = true;
-			//if (col_ava) {
-			//	//isDied = true;
-			//}
 		}
 	}
 }
